c33874fe7a99e2d88a99e036c5881c2a091603aa
DarioAlessioR <100212928+DarioAlessioR@users.noreply.github.com>
2022-06-09 18:21:46 -0400

    Fix linters errors in index js and config js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
+/* eslint-disable linebreak-style */
 import _ from 'lodash';
 import './style.css';
 
 function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    element.classList.add('hello');
+  const element = document.createElement('div');
 
-    return element;
-  }
-  
-  document.body.appendChild(component());
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  element.classList.add('hello');
+
+  return element;
+}
+
+document.body.appendChild(component());
