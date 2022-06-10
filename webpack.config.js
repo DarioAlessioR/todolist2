0d69a58d81ee04a472826c2dffb6c67318a05d18
DarioAlessioR <100212928+DarioAlessioR@users.noreply.github.com>
2022-06-10 10:21:32 -0400

    put

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,16 @@
 /* eslint-disable linebreak-style */
-const path = require('path');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
-  devServer: {
-    static: './dist',
-  },
-  devtool: 'inline-source-map',
+  
+
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
   ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  optimization: {
-    runtimeChunk: 'single',
-  },
   module: {
     rules: [
       {
