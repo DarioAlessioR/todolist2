d17df2e901e261d88747f8b8a1d24b604c49f813
DarioAlessioR <100212928+DarioAlessioR@users.noreply.github.com>
2022-06-13 10:25:40 -0400

    fix linter errors

--- src/index.js
+++ src/index.js
@@ -15,38 +15,38 @@ const showtodolist = (todolist) => {
   const totallist = document.getElementById('totallist');
   totallist.innerHTML = '';
   for (let i = todolist.length - 1; i > 0; i -= 1) {
-// Create a line div to put inside checkbox, activity (todo) and icon
+    // Create a line div to put inside checkbox, activity (todo) and icon
     const line = document.createElement('div');
     line.setAttribute('class', 'linelist');
-// Create a leftside div to put inside checkbox and activity 
+    // Create a leftside div to put inside checkbox and activity
     const leftside = document.createElement('div');
     leftside.setAttribute('id', 'leftside');
-// Create the todo in activity
+    // Create the todo in activity
     const activity = document.createElement('p');
     activity.setAttribute('contentEditable', 'true');
     activity.setAttribute('class', `${todolist[i].id}`);
     activity.innerHTML = `${todolist[i].todo}`;
-// Make activity field editable and save new content to array and localStorage
-    activity.addEventListener('keydown', function(e) {
+    // Make activity field editable and save new content to array and localStorage
+    activity.addEventListener('keydown', () => {
       const todolist = JSON.parse(localStorage.getItem('texttodolist'));
       todolist[i].todo = activity.innerHTML;
-      localStorage.setItem('texttodolist', JSON.stringify(todolist));  
+      localStorage.setItem('texttodolist', JSON.stringify(todolist));
     });
-// Create checkbox
+    // Create checkbox
     const checkbox = document.createElement('input');
     checkbox.setAttribute('type', 'checkbox');
     checkbox.setAttribute('name', 'checkbox');
     checkbox.setAttribute('class', `${todolist[i].id}`);
-// Create divicon3 div to put inside icons
+    // Create divicon3 div to put inside icons
     const divicon3 = document.createElement('div');
     divicon3.setAttribute('class', 'change');
-// Create three dots icon and change it to trash can icon on click
+    // Create three dots icon and change it to trash can icon on click
     const icon3 = document.createElement('i');
     icon3.setAttribute('class', 'fa-solid fa-ellipsis-vertical');
     divicon3.addEventListener('click', () => {
       icon3.setAttribute('class', 'fa-solid fa-trash-can');
-// Give the trash can icon the order to delete the i element on click. After deletenig, will save new array
-//   and show new list of activities on screen 
+      // Give the trash can icon the order to delete the i element on click. After deletenig,
+      //   will save new array and show new list of activities on screen
       icon3.addEventListener('click', () => {
         const deletetodo = `${i}`;
         const todolist = JSON.parse(localStorage.getItem('texttodolist'));
@@ -58,11 +58,11 @@ const showtodolist = (todolist) => {
         return showtodolist(todolist);
       });
     });
-// Appends all elements in this order:
-// (icon3) --> (divicon3)
-// (checkbox) and (activity) --> (leftside)
-// (leftside) and (divicon3) --> (line)
-// (line) -- (totallist)
+    // Appends all elements in this order:
+    // (icon3) --> (divicon3)
+    // (checkbox) and (activity) --> (leftside)
+    // (leftside) and (divicon3) --> (line)
+    // (line) -- (totallist)
     divicon3.appendChild(icon3);
     leftside.appendChild(checkbox);
     leftside.appendChild(activity);
@@ -98,7 +98,8 @@ const addtodo = (inputtodo) => {
         todostatus: true,
       },
     );
-// After updating the array, save it to localStorage and call showtodolist function to show new list on screen
+    // After updating the array, save it to localStorage and call showtodolist function to
+    //  show new list on screen
     localStorage.setItem('texttodolist', JSON.stringify(todolist));
   } showtodolist(JSON.parse(localStorage.getItem('texttodolist')));
 };
