405507febad68f14508811227bb841b1109a85cf
DarioAlessioR <100212928+DarioAlessioR@users.noreply.github.com>
2022-06-14 23:28:20 -0400

    fix more linter errors in js changestatus

--- src/modules/changestatus.js
+++ src/modules/changestatus.js
@@ -2,13 +2,13 @@
 //  array for checked elements
 const changeStatus = (i) => {
   const todolist = JSON.parse(localStorage.getItem('texttodolist'));
-  if (todolist[i].todostatus == true) {
-    todolist[i].todostatus = false
-    localStorage.setItem('texttodolist', JSON.stringify(todolist));  
-  } else {
-    todolist[i].todostatus = true
+  if (todolist[i].todostatus === true) {
+    todolist[i].todostatus = false;
     localStorage.setItem('texttodolist', JSON.stringify(todolist));
-  };
+  } else {
+    todolist[i].todostatus = true;
+    localStorage.setItem('texttodolist', JSON.stringify(todolist));
+  }
 };
 
 export default changeStatus;
