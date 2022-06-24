d7ec25dc5698c52d050debd8b1ece196f754ebb4
Maury PC <tryptic-st@outlook.com>
2022-06-23 20:45:20 -0500

    Chek linter errors

--- src/__mock__/addtodo.js
+++ src/__mock__/addtodo.js
@@ -1,18 +1,11 @@
-
-
-
 const addtodo = (inputtodo) => {
-let todolist = [];
-      todolist.push(
-        {
-          id: todolist.length > 0 ? todolist[todolist.length - 1].id + 1 : 1,
-          todo: inputtodo,
-          todostatus: true,
-        },
-      );
-      console.log(todolist)
-    }
-
- //addtodo('hhhhhhhh');
-  export default addtodo;
-  
+  const todolist = [];
+  todolist.push(
+    {
+      id: todolist.length > 0 ? todolist[todolist.length - 1].id + 1 : 1,
+      todo: inputtodo,
+      todostatus: true,
+    },
+  );
+};
+export default addtodo;
